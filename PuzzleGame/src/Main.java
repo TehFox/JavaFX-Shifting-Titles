5880c1c40652af4923b55a6ae64c83cfb6aed266
Vitor-TPSI <Vitor-TPSI@10.0.2.15>
2016-09-08 21:26:15 +0100

    gh

--- PuzzleGame/src/Main.java
+++ PuzzleGame/src/Main.java
@@ -7,6 +7,9 @@ import javafx.scene.control.*;
 import javafx.geometry.*;
 import javafx.event.*;
 import javax.swing.JOptionPane;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 
 
 //Public class
@@ -17,8 +20,8 @@ public class Main extends Application{
 	private static boolean vez=true;
 	int numero=0;
 	GridPane grid;
-	Button button; 
-	
+	Button button;
+
 	//main
 	public static void main(String [] args)
 	{
@@ -29,17 +32,31 @@ public class Main extends Application{
 	//metodo start
 	public void start(Stage myStage)
 	{
-		
-		
+
+
 		myStage.setTitle("JavaFX");
 		Group root= new Group();
 		Scene myScene = new Scene(root,500,500,Color.LIGHTBLUE);//definir tamanho da frame
-		
+
 		grid= new GridPane();
-		
+	 	MenuBar menuBar = new MenuBar();
+	 	//menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
+  		//.setTop(menuBar);
+
+  		 Menu fileMenu = new Menu("File");
+		    MenuItem newMenuItem = new MenuItem("New");
+		    MenuItem saveMenuItem = new MenuItem("Save");
+		    MenuItem exitMenuItem = new MenuItem("Exit");
+		    exitMenuItem.setOnAction(actionEvent -> Platform.exit());
+
+		    fileMenu.getItems().addAll(newMenuItem, saveMenuItem,
+		        new SeparatorMenuItem(), exitMenuItem);
+
+
+
 		//grid.setMaxSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
 		//grid.setMaxSize(500, 500);
-		
+
 		for(int i=0;i<4;i++)//Coluna
 		{
 			ColumnConstraints coluna = new ColumnConstraints();
@@ -56,12 +73,12 @@ public class Main extends Application{
 				button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 				//button.setPrefWidth(200);
 				//button.setPrefHeight(200);
-				
+
 				if(numero!=16)
 					button.setText(""+numero);
 				else
 					button.setText("");
-				
+
 				button.setStyle("-fx-font-family:verdana;-fx-font-size:14px;-fx-text-fill:blue;-fx-background-color:white;-fx-border-color:black;");
 				 grid.add(button, i, a);
 			     //grid.getChildren().add(button);
@@ -69,15 +86,17 @@ public class Main extends Application{
 			grid.getColumnConstraints().add(coluna);
 			 grid.getRowConstraints().add(linha);
 		}
-	     
+
 		root.getChildren().add(grid);
 
 
 		/*PROGRAMAÇÃO*/
-		
-		
-		
-		
+
+
+
+
+
+
 		myStage.setScene(myScene);
 		myStage.show();
 	}
