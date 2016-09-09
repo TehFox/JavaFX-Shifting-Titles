7caf552472af203dcab0c3365b7b40cfb4d7270b
Marco <Marco@10.0.2.15>
2016-09-09 17:53:08 +0100

    Botões dinamicos

--- PuzzleGame/src/Main.java
+++ PuzzleGame/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -22,9 +24,9 @@ public class Main extends Application {
 	private String []Romano= {"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI"};
 	private String []Letra={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"};
     private GridPane gridpane = new GridPane();
-    private static celula [] listacelulas;
-    private static final int MAXCELULAS=16;
-    private Button botao;
+    private static ArrayList<celula> listacelulas= new ArrayList<celula>();
+   // private static final int MAXCELULAS=16;
+    private Button [] botao= new Button[16];
     String valor;
     boolean letra=false;
     boolean romano=false;
@@ -32,7 +34,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-    	listacelulas= new celula[MAXCELULAS];
 
 		MenuBar menuBar = new MenuBar();
         Pane root = new Pane();
@@ -44,71 +45,39 @@ public class Main extends Application {
 			    public void handle(ActionEvent t) {
 			        letra=true;
 			        valid=false;
-			        root.getChildren().remove(botao);
+			        root.getChildren().remove(gridpane);
+			        botoes();
 			    }
 });
-		    //letraMenuItem.setOnAction(actionEvent -> letra=true;valid=false);
 		    MenuItem romanoMenuItem = new MenuItem("Romano");
-		    //romanoMenuItem.setOnAction(actionEvent -> romano=true;valid=false);
 		    romanoMenuItem.setOnAction(new EventHandler<ActionEvent>() {
 						    public void handle(ActionEvent t) {
 						        romano=true;
-						        valid=false;
-						        root.getChildren().remove(botao);
+						        root.getChildren().remove(gridpane);
+						        botoes();
+
 						    }
 });
-		    fileMenu.getItems().addAll(letraMenuItem,
+
+MenuItem numeroMenuItem = new MenuItem("Numero");
+		    //romanoMenuItem.setOnAction(actionEvent -> romano=true;valid=false);
+		    numeroMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+						    public void handle(ActionEvent t) {
+						        romano=false;
+						        letra=false;
+						        root.getChildren().remove(gridpane);
+						        botoes();
+
+						    }
+});
+
+botoes();
+		    fileMenu.getItems().addAll(letraMenuItem,numeroMenuItem,
         new SeparatorMenuItem(), romanoMenuItem);
 
 		Menu inforMenu = new Menu("Informação");
 		MenuItem classifiMenuItem = new MenuItem("Classificação");
 		inforMenu.getItems().addAll(classifiMenuItem);
-System.out.println("PUTA");
-
-
-		do{
-		 for(int i=0;i<4;i++)//Coluna
-			{
-				for(int a=0;a<4;a++)
-				{
-					System.out.printf("CRL");
-					contador++;
-
-					if(contador!=16){
-						if(romano==true){
-						botao = new Button("" + Romano[contador]);}
-						if(letra==true){
-							botao = new Button("" + Letra[contador]);}
-							if(romano==false&&letra==false){
-								botao = new Button("" + contador);
-							}
-					}
-
-					else
-					{
-						botao = new Button("");
-					}
-			        final int numButton= contador;
-			        final String numero;
-			        numero="" + contador;
-			        botao.setId(""+contador);
-			        celula cel= new celula(numButton, numero, false, false);
-			        botao.setPrefSize(150, 150);
-			        botao.setStyle("-fx-font-size:42px");
-			        botao.setOnAction(new EventHandler<ActionEvent>() {
-			            @Override
-			            public void handle(ActionEvent e) {
-			                System.out.println("id(" + botao.getId() + ") =  " + numButton);
-			            }
-			        });
-			        gridpane.add(botao, i, a);
-				}
-			}
-
-valid=true;
-}while(valid==false);
-			romano=false;
-			letra=false;
 		menuBar.getMenus().addAll(fileMenu,inforMenu);
         border.setTop(menuBar);
         border.setCenter(gridpane);
@@ -121,4 +90,58 @@ valid=true;
     public static void main(String[] args) {
         launch(args);
     }
+
+    void botoes(){
+		contador=0; 
+		
+				 for(int i=0;i<4;i++)//Coluna
+					{
+						for(int a=0;a<4;a++)
+						{
+							
+							contador++;
+							if(contador<16){
+								
+								if(romano==true){
+								botao[contador-1] = new Button("" + Romano[contador-1]);
+								celula cel= new celula(contador-1, Romano[contador-1], false, false);
+								listacelulas.add(cel);
+								}
+								
+								if(letra==true){
+									botao[contador-1] = new Button("" + Letra[contador-1]);
+									celula cel1= new celula(contador-1, Letra[contador-1], false, false);
+									listacelulas.add(cel1);
+									}
+								
+									if(romano==false&&letra==false){
+										botao[contador-1] = new Button("" + contador);
+										celula cel2= new celula(contador-1, String.valueOf(contador), false, false);
+										listacelulas.add(cel2);
+										}
+							}
+							else
+							{
+								botao[contador-1] = new Button("");
+								celula cel2= new celula(contador-1, String.valueOf(contador), false, true);
+								listacelulas.add(cel2);
+							}
+							
+							
+							
+					        final int numButton= contador;
+					        final String numero;
+					        numero="" + contador;
+					        
+					        botao[contador-1].setPrefSize(150, 150);
+					        botao[contador-1].setStyle("-fx-font-size:42px");
+					        
+					        gridpane.add(botao[contador-1], i, a);
+						}
+					}
+letra=false;
+	romano=false;
+	}
+
+
 }
